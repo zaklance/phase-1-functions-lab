440675fc2616568cc700440279bd53ce3b7b427d
zaklance <wosewick@gmail.com>
2024-04-20 15:49:24 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(42 - street);
+}
+
+function distanceFromHqInFeet(count) {
+    let blocks = distanceFromHqInBlocks(count);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let dist = Math.abs(start - destination) * 264;
+    if(dist < 400, dist <= 2000){
+        let value = (dist - 400) * 0.02;
+        return value = value < 0 ? 0 : value;
+    } else if(dist < 2000, dist <= 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+};
